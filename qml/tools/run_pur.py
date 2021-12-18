f679304b6c7e2eb9eaf38f9e21677854dcaaf50e
paiforsyth <paiforsyth@hotmail.com>
2021-12-18 14:57:02 -0500

    check up to date in ci

--- qml/tools/run_pur.py
+++ qml/tools/run_pur.py
@@ -1,7 +1,17 @@
+import argparse
+import sys
+
 from pur import update_requirements
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--check", action="store_true")
+    args = parser.parse_args()
     messages = [
         x[0]["message"]
-        for x in update_requirements(input_file="requirements.txt").values()
+        for x in update_requirements(
+            input_file="requirements.txt", dry_run=args.check
+        ).values()
     ]
+    if len(messages) > 0:
+        sys.exit(1)
