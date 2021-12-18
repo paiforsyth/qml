13d68283573f55cfaf561f87013fb6bb9efafb8b
paiforsyth <paiforsyth@hotmail.com>
2021-12-18 15:20:19 -0500

    exclude setuptools from pur

--- qml/tools/run_pur.py
+++ qml/tools/run_pur.py
@@ -9,7 +9,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--check", action="store_true")
     args = parser.parse_args()
-    result = update_requirements(input_file="requirements.txt", dry_run=args.check)
+    result = update_requirements(
+        input_file="requirements.txt",
+        dry_run=args.check,
+        skip=["types-setuptools", "setuptools"],
+    )
     updated = [x for x in result.values() if x[0]["updated"]]
     num_updated = len(updated)
     if num_updated > 0:
