009cef0d5c5504e3bdd838c0ee832ff0fb1c6d14
paiforsyth <paiforsyth@hotmail.com>
2022-01-30 11:15:26 -0500

    version

--- nuke_cache_if_too_big.sh
+++ nuke_cache_if_too_big.sh
@@ -20,3 +20,7 @@ echo "contents of python cache"
 ls -l /home/runner/.cache/pants/setup/bootstrap-Linux-x86_64/pants.eTLSta/install
 echo "contents of python cache bin"
 ls -l /home/runner/.cache/pants/setup/bootstrap-Linux-x86_64/pants.eTLSta/install/bin
+echo "python versiom"
+/opt/hostedtoolcache/Python/3.9.9/x64/bin/python3.9 --version
+echo "python version through symlink"
+/home/runner/.cache/pants/setup/bootstrap-Linux-x86_64/2.8.0_py39/bin/python --version
